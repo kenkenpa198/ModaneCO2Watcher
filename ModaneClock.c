19ea9f14f7154099edffe2e5deccb9673222764d
kenkenpa198 <kenkenpa198@gmail.com>
2022-06-12 12:51:47 +0900

    [update][add]コードの整理。gitignore と readme の追加

--- ModaneClock.c
+++ ModaneClock.c
@@ -8,65 +8,92 @@ typedef char String[1024];
 
 int main(void) {
 
+    // もだねちゃんの顔を各変数へ格納
+    String modaneTop =     "                          \n"
+                           "                ____      \n"
+                           "              /      \\   \n"
+                           "             ( ____   ﾚ-、\n"
+                           "           ／       -ノ_ﾗ \n"
+                           "          \"  人      ヽ  \n"
+                           "        /  ／_ `-_     \\ \n"
+                           "       |  /        `-_  | ";
+
+    String modaneEye01 =   "       (||   |    |   ||) ";
+    String modaneEye02 =   "       (|| -==    ==- ||) ";
+
+    String modaneMouth01 = "        \\) ,,  ー  ,, (ﾉ  ";
+    String modaneMouth02 = "        \\) ,,  ワ  ,, (ﾉ  ";
+
+    String modaneJaw =     "         )ヽ________ノ(   ";
+    String modaneBottom =  "             \\_父_/\n    ";
+
+
+    // 時計の表示処理
     srand((unsigned)time(NULL));
+    char date[64], dow[64], time_[64];
+    time_t prev, now;
 
-    String modaneTop =    "                          \n"
-                          "                ____      \n"
-                          "              /      \\   \n"
-                          "             ( ____   ﾄ-、\n"
-                          "           ／       -ノ_ﾗ \n"
-                          "          \"  人      ヽ  \n"
-                          "        /  ／_ `-_     \\ \n"
-                          "       |  /        `-_  | ";
-                                  //   目と口は
-                                  // 乱数で出し分け
-    String modaneJaw =    "         )ヽ________ノ(   ";
-    String modaneBottom = "             \\_父_/\n    ";
-
+    // 無限に繰り返し
     while(true) {
 
-        char date[64], dow[64], time_[64];
-        time_t prev, now;
-
+        // 現在日時と繰り返し前の値を比較して同じなら処理をコンティニューする
         now = time(NULL);
-
         if (prev == now) {
             continue;
         }
-        prev = now;
+        prev = now; // 現在日時を次の比較用に格納
 
+        // 日付と曜日の取得
         strftime(date, sizeof(date), "%Y-%m-%d %a.", localtime(&now));
         strftime(dow, sizeof(dow), "%A", localtime(&now));
 
+        // 時間を取得。偶奇でコロンの有無を場合分け
         if (now % 2 == 0) {
             strftime(time_, sizeof(time_), "%H:%M", localtime(&now));
         } else {
             strftime(time_, sizeof(time_), "%H %M", localtime(&now));
         }
 
+        // シェルの表示を消去する
+        // TODO: Windows でも動かせるようにする？
         system("clear");
+
+        // もだねちゃんと日時を表示
+        // TODO: 関数化して一気に表示するようにする
+
+        // 乱数生成
+        int eyeNum = rand()%2;
+        int mouthNum = rand()%2;
+
+        // 頭の表示
         printf("%s\n", modaneTop);
 
-        int eyeNum = rand()%11;
-        if (eyeNum >= 5) {
-            String modaneEye   =    "       (||   |    |   ||) ";
-            printf("%s   %s\n", modaneEye, date);
-        } else {
-            String modaneEye   =    "       (|| -==    ==- ||) ";
-            printf("%s   %s\n", modaneEye, date);
+        // 目と日付の表示
+        switch (eyeNum) {
+            case 0:
+                printf("%s   %s\n",              modaneEye01, date);
+                break;
+            case 1:
+                printf("%s   %s\n",              modaneEye02, date);
+                break;
         }
 
-        int mouthNum = rand()%11;
-        if (mouthNum >= 5) {
-            String modaneMouth =    "        \\) ,,  ー  ,, (ﾉ  ";
-            printf("%s   ---------------\n", modaneMouth);
-        } else {
-            String modaneMouth =    "        \\) ,,  ワ  ,, (ﾉ  ";
-            printf("%s   ---------------\n", modaneMouth);
+        // 口と区切り線の表示
+        switch (mouthNum) {
+            case 0:
+                printf("%s   ---------------\n", modaneMouth01);
+                break;
+            case 1:
+                printf("%s   ---------------\n", modaneMouth02);
+                break;
         }
 
-        printf("%s        %s\n", modaneJaw, time_);
-        printf("%s\n", modaneBottom);        sleep(1);
+        // アゴと時間の表示
+        printf("%s        %s\n",         modaneJaw, time_);
+
+        // 首下の表示
+        printf("%s\n",                   modaneBottom);
     }
+
     return 0;
 }
---
+++ README.md
@@ -0,0 +1,10 @@
+<!-- omit in toc -->
+# もだねちゃんクロック
+
+## 1. 概要
+
+TODO: 作成する！
+
+## 2. 参考サイト
+
+- [C言語ケーススタディ　時計の作り方1](http://www.orchid.co.jp/computer/cschool/clock1.html)
