d500653aebe2bd349a4807ca19e41b050b4398a3
kenkenpa198 <kenkenpa198@gmail.com>
2022-06-30 21:28:47 +0900

    [update]co2 取得用の python を作成

--- get_co2.py
+++ get_co2.py
@@ -1,3 +1,17 @@
-import mh_z19
+import csv
+from datetime import datetime as dt
+# import mh_z19
 
-CO2_conce = mh_z19.read_all()["co2"]
+# 現在日時を取得（YYYY-MM-DD hh:mm:ss）
+now = dt.now().strftime('%Y-%m-%d %H:%M:%S')
+print(now)
+
+# CO2 濃度の値を取得
+# co2_conce = mh_z19.read_all()["co2"]
+co2_conce = 111
+print(co2_conce)
+
+# CSV ファイルへ書き込み
+with open('./logs/co2_conces.csv', 'a') as f:
+    writer = csv.writer(f)
+    writer.writerow([now, co2_conce])
