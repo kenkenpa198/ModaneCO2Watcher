e47174aef0fc542f43f82a69ca11f980a80de419
kenkenpa198 <kenkenpa198@gmail.com>
2022-07-01 23:22:55 +0900

    [update]CO2 濃度配列を取得する関数の実装。コメントまわりの整理

--- ModaClock.c
+++ ModaClock.c
@@ -1,27 +1,31 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#include <unistd.h>
 
-#include <ncurses.h> // curses ライブラリ
-#include <locale.h>  // curses で2バイト文字を扱えるようにする
+#include <locale.h>  // setlocale
+#include <stdlib.h>  // system, getenv, srand, rand, maclloc, exit
+#include <string.h>  // strlen
+#include <time.h>    // time, localtime, strftime
+#include <unistd.h>  // sleep, usleep
+
+#include <ncurses.h> // curses
 
 
 /***************************************
-CO2 濃度をログへ書き込む関数
-
-記録用 Python ファイルを実行する関数。
-Python ファイル内部で CO2 濃度を測定とログ用 CSV への書き込み処理を行う。
-
-引数   : なし
-戻り値 : なし
-
-// TODO: 下記を検証する
-- python ファイルが存在しなかった場合
-- python ファイルが権限エラーなどで実行できなかった場合
-- python ファイルの実行中に何らかのエラーが起こった場合
-- python ファイルの中で呼び出している mh_z19 のセンサーが起動していなかった場合
+ * CO2 濃度をログへ書き込む関数
+ * 
+ * 記録用 Python ファイルを実行する関数。
+ * Python ファイル内部で CO2 濃度を測定とログ用 CSV への書き込み処理を行う。
+ * 
+ * ▼引数
+ * なし
+ * 
+ * ▼戻り値
+ * なし
+ * 
+ * TODO: 下記を検証する
+ * - python ファイルが存在しなかった場合
+ * python ファイルが権限エラーなどで実行できなかった場合
+ * python ファイルの実行中に何らかのエラーが起こった場合
+ * python ファイルの中で呼び出している mh_z19 のセンサーが起動していなかった場合
 
 ***************************************/
 void doRecordCo2ConceToLogs(void) {
@@ -30,59 +34,83 @@ void doRecordCo2ConceToLogs(void) {
 }
 
 /***************************************
-CO2 濃度をログから取得する関数
-
-CO2 濃度のログファイル最終行（最新）から CO2 濃度を取得して返却する。
-引数で指定された数値分、行を巻き戻る。
-
-引数   : 最終行から遡る行数
-戻り値 : 遡った先に行が存在した場合 → CO2 濃度の数値
-         存在しなかった場合         → NULL
-
+ * CO2 濃度をログから取得して配列を書き換える関数
+ * CO2 濃度のログファイル最終行（最新の記録）から 6行飛ばし（1時間毎）に CO2 濃度を取得し、CO2 の濃度配列へ再代入する。
+ * 再代入配列の後ろから行う。
+ * 
+ * 引数の指定がある場合、その行数分遡った先にある値を取得する。
+ * 取得先の行が存在しなかった場合、 co2Conces の対応する要素は書き変わらない。
+ * ファイルの展開に失敗した場合は何もしない。
+ * 
+ * TODO: ファイルの展開に失敗した場合、-1 に置き換えたり mvaddstr でメッセージを表示する？
+ * 
+ * ▼引数
+ * int co2Conces[21] : CO2 濃度配列のポインタ
+ * 
+ * ▼戻り値
+ * なし
+ * 
 ***************************************/
-int getCo2ConceFromLogs(int returnRowCount) {
-    // TODO: CSV ファイルを読み込む
-    // TODO: 引数分だけ最終行から行を遡ってその値を取得する
-    // TODO: 遡る先の行が存在しなかった場合は NULL を返す
+void getCo2ConcesFromLogs(int co2Conces[21]) {
+    // ログファイルの展開
+    char filepath[128] = "./logs/co2_conces.csv";
+    FILE * fp;
+    fp = fopen(filepath, "r");
 
-    // 作成用
-
-    int co2Conce = rand() % 1600 + 1;
-
-    return co2Conce;
-}
-
-/***************************************
-CO2 濃度の配列を作成する関数
-
-「CO2 濃度をログから取得する関数」をループ処理し、
-○分毎の濃度を配列として格納する。
-
-引数   : CO2 濃度の配列？
-戻り値 : CO2 濃度の配列
-
-***************************************/
-void getAndAssignCo2Conces(int co2Conces[21]) {
-    // TODO: CSV ファイルを読み込む
-    // TODO: 引数分だけ最終行から行を遡ってその値を取得する
-    // TODO: 遡る先の行が存在しなかった場合は NULL を返す
-
-    int n = 0;
-    for (int i = 0; i < 21; i++) {
-        co2Conces[i] = getCo2ConceFromLogs(n);
-        n += 6; // TODO: 10 * ○分毎の指定。10分毎に記録するので、6 で指定すると6行分ずつ遡って格納する 
+    // ファイルの展開に失敗した場合の処理
+    if (!fp) {
+        // TODO: ファイルの展開に失敗した場合、-1 に置き換えたり mvaddstr でメッセージを表示する？
+        fclose(fp);
+        return;
     }
+
+    // 取得用変数・配列の宣言
+    int size = 64;        // バッファのサイズ（要素数）
+    char buf[size];       // 取得データ格納先へのポインタ
+    int lineCount = 0;    // 行カウント
+    int co2Conce;         // CO2 濃度値
+    char gotDatetime[64]; // 日付。ただし格納するけど使わない。省きたい…
+
+    // ヒープを確保
+    int* co2ConcesHeap;
+    co2ConcesHeap = (int*)malloc(sizeof(int) * 60000); // 1年分くらい
+    if (co2ConcesHeap == NULL) {
+        // ヒープが確保できなかったらプログラムを強制終了する
+        exit(0);
+    }
+
+    // 行数を数えつつ CO2 濃度をヒープ配列へ代入
+    for (int i = 0; fgets(buf, size, fp) != NULL; i++) {
+        sscanf(buf, "%d, %[^\n]", &co2Conce, gotDatetime);
+        co2ConcesHeap[i] = co2Conce;
+        lineCount++;
+    }
+    fclose(fp);
+
+    for (int i = 0; i <lineCount; i++) {
+        printf("%d\n", co2ConcesHeap[i]);
+    }
+
+    // ヒープ配列の最終要素から6個毎（1時間毎）に CO2 配列の最終要素から順に再代入する
+    int c = lineCount - 1; // lineCount を添え字に合わせる
+    for (int i = 20; i >= 0 && c >= 0; i--) {
+        co2Conces[i] = co2ConcesHeap[c];
+        c -= 6;
+    }
+    free(co2ConcesHeap); // ヒープの解放
 
     return;
 }
 
-
 /***************************************
-天気取得コマンドを作成する関数
-
-引数   : コマンドを格納する既定の配列
-戻り値 : なし（既定の配列を書き換えるため）
-
+ * 天気取得コマンドを作成する関数
+ * 
+ * ▼引数
+ * char wttrCmd[512] : 天気取得コマンド配列のポインタ
+ * 
+ * ▼戻り値
+ * なし
+ * 
 ***************************************/
 void makeWttrCmd(char wttrCmd[512]) {
     // 取得するロケールを初期化
@@ -109,11 +137,15 @@ void makeWttrCmd(char wttrCmd[512]) {
 }
 
 /***************************************
-天気を取得する関数
-
-引数   : 天気取得コマンドの配列, 天気情報を格納する既定の二次元配列（512バイト × 2）
-戻り値 : なし（既定の配列を書き換えるため）
-
+ * 天気を取得する関数
+ * 
+ * ▼引数
+ * char wttrCmd[512]      : 天気取得コマンド配列のポインタ
+ * char wttrLines[2][512] : 天気情報を格納する二次元配列ポインタ（512バイト × 2）
+ * 
+ * ▼戻り値
+ * なし
+ * 
 ***************************************/
 void getWttrLines(char wttrCmd[512], char wttrLines[2][512]) {
     // 天気の格納用配列を宣言
@@ -131,11 +163,16 @@ void getWttrLines(char wttrCmd[512], char wttrLines[2][512]) {
 }
 
 /***************************************
-描画用関数 : 現在時刻を毎秒描画する
-
-引数   : Y 座標, X 座標, time_t 型の現在日時
-戻り値 : なし
-
+ * 描画用関数 : 現在時刻を毎秒描画する
+ * 
+ * ▼引数
+ * int y      : 描画を行う Y 座標
+ * int x      : 描画を行う X 座標
+ * time_t now : time(NULL) で取得できる UNIX 時間
+ * 
+ * ▼戻り値
+ * なし
+ * 
 ***************************************/
 void printDatetime(int y, int x, time_t now) {
     char datetime[64];
@@ -155,11 +192,16 @@ void printDatetime(int y, int x, time_t now) {
 }
 
 /***************************************
-描画用関数 : 天気を描画する
-
-引数   : Y 座標, X 座標, 天気文字列の二次元配列
-戻り値 : なし
-
+ * 描画用関数 : 天気を描画する
+ * 
+ * ▼引数
+ * int y                  : 描画を行う Y 座標
+ * int x                  : 描画を行う X 座標
+ * char wttrLines[2][512] : 天気文字列の二次元配列のポインタ
+ * 
+ * ▼戻り値
+ * なし
+ * 
 ***************************************/
 void printWttr(int y, int x, char wttrLines[2][512]) {
     for (int i = 0; i < 2; i++) {
@@ -169,11 +211,18 @@ void printWttr(int y, int x, char wttrLines[2][512]) {
 }
 
 /***************************************
-描画用関数 : CO2グラフの軸を描画する
-
-引数   : Y 座標, X 座標, time_t 型の現在日時
-戻り値 : なし
-
+ * 描画用関数 : CO2グラフの軸を描画する
+ * 
+ * ▼引数
+ * int y      : 描画を行う Y 座標
+ * int x      : 描画を行う X 座標
+ * time_t now : time(NULL) で取得できる UNIX 時間
+ * 
+ * ▼戻り値
+ * なし
+ * 
+ * TODO: キー入力を受け取ったらグラフの表示を可変したい
+ * 
 ***************************************/
 void printCo2GraphBase(int y, int x) {
     char co2GraphBaseLines[7][64] = {
@@ -194,13 +243,18 @@ void printCo2GraphBase(int y, int x) {
 }
 
 /***************************************
-描画用関数 : CO2グラフの折れ線グラフを描画する
-
-引数   : Y 座標, X 座標, CO2 濃度の配列
-戻り値 : なし
-
+ * 描画用関数 : CO2グラフの折れ線グラフを描画する
+ * 
+ * ▼引数
+ * int y             : 描画を行う Y 座標
+ * int x             : 描画を行う X 座標
+ * int co2Conces[21] : CO2 濃度配列
+ * 
+ * ▼戻り値
+ * なし
+ * 
 ***************************************/
-void printCo2LineGraph(int y, int x, int co2Conces[11]) {
+void printCo2LineGraph(int y, int x, int co2Conces[21]) {
     // 境界値配列の初期化
     // 下限値値（250）+ 2000 / 12 の値を切り捨てた値（167）* 繰り返し回数（i）で境界値を計算
     // NOTE: 切り捨て時の端数を考慮していないため、少しずつ境界値が本来の境界値よりズレていく
@@ -243,11 +297,16 @@ void printCo2LineGraph(int y, int x, int co2Conces[11]) {
 }
 
 /***************************************
-描画用関数 : CO2グラフの現在の値を描画
-
-引数   : Y 座標, X 座標, 現在のCO2濃度
-戻り値 : なし
-
+ * 描画用関数 : CO2グラフ右側へ現在の値を描画する
+ * 
+ * ▼引数
+ * int y           : 描画を行う Y 座標
+ * int x           : 描画を行う X 座標
+ * int co2ValueNow : 現在の CO2 濃度
+ * 
+ * ▼戻り値
+ * なし
+ * 
 ***************************************/
 void printCo2ValueNow(int y, int x, int co2ValueNow) {
         if (co2ValueNow < 750) {
@@ -263,11 +322,16 @@ void printCo2ValueNow(int y, int x, int co2ValueNow) {
 }
 
 /***************************************
-描画用関数 : CO2グラフを1秒毎に点滅させる
-
-引数   : Y 座標, X 座標, time_t 型の現在日時
-戻り値 : なし
-
+ * 描画用関数 : CO2グラフを1秒毎に点滅させる
+ * 
+ * ▼引数
+ * int y           : 描画を行う Y 座標
+ * int x           : 描画を行う X 座標
+ * int co2ValueNow : time_t now : time(NULL) で取得できる UNIX 時間
+ * 
+ * ▼戻り値
+ * なし
+ * 
 ***************************************/
 void doBlinkCo2Graph(int y, int x, time_t now) {
     if (now % 2 == 0) {
@@ -280,11 +344,17 @@ void doBlinkCo2Graph(int y, int x, time_t now) {
 }
 
 /***************************************
-描画用関数 : もだねちゃん AA を描画する
-
-引数   : Y 座標, X 座標, 目の乱数, 口の乱数
-戻り値 : なし
-
+ * 描画用関数 : もだねちゃん AA を描画する
+ * 
+ * ▼引数
+ * int y        : 描画を行う Y 座標
+ * int x        : 描画を行う X 座標
+ * int eyeNum   : 表示する目の添え字
+ * int mouthNum : 表示する口の添え字
+ * 
+ * ▼戻り値
+ * なし
+ * 
 ***************************************/
 void printModane(int y, int x, int eyeNum, int mouthNum) {
     // AA 用配列を初期化
@@ -345,40 +415,31 @@ void printModane(int y, int x, int eyeNum, int mouthNum) {
 
 
 int main(void) {
-    ////////// タイトル表示 //////////
+    /***************************************
+     * タイトル表示
+    ***************************************/
     printf("------------------------------------------\n");
     printf("                Moda Clock                \n");
     printf("------------------------------------------\n");
 
 
-    ////////// CO2 グラフの描画処理準備 //////////
-    // TODO: 別ファイルから濃度の値を取得する関数をこの辺で実行。時系列の昇順で格納する（1番目が21分前、20番目が1分前、21番目が現在）
-
+    /***************************************
+     * CO2 グラフの描画処理準備
+    ***************************************/
     // ログファイルから過去の CO2 濃度を取得して配列へ格納
-    srand((unsigned)time(NULL)); // TODO: 処理が完成したら消す
     int co2Conces[21] = {0};
-    getAndAssignCo2Conces(co2Conces);
-
-    // TODO: 作成終わったら消す
-    int i, j, tmp;
-    for (i=0; i<sizeof(co2Conces)/sizeof(co2Conces[0]); i++) {
-        for(j=i+1; j<sizeof(co2Conces)/sizeof(co2Conces[0]); j++) {
-            if(co2Conces[j] < co2Conces[i]) {
-                tmp = co2Conces[i];
-                co2Conces[i] = co2Conces[j];
-                co2Conces[j] = tmp;
-            }
-        }
-    }
+    getCo2ConcesFromLogs(co2Conces);
 
     // 取得したCO2配列の確認
     for (int i = 0; i < 21; i++) {
-        printf("co2Conces %2d : %4d\n", i, co2Conces[i]);
+        printf("co2Conces[%2d] : %4d\n", i, co2Conces[i]);
     }
     printf("\n");
 
 
-    ////////// 天気の描画処理準備 //////////
+    /***************************************
+     * 天気の描画処理準備
+    ***************************************/
     // wttr コマンドの作成
     char wttrCmd[512];
     makeWttrCmd(wttrCmd);
@@ -390,13 +451,17 @@ int main(void) {
     printf("wttrLines :\n%s%s\n\n", wttrLines[0], wttrLines[1]);
 
 
-    ////////// もだねちゃんの描画処理準備 //////////
+    /***************************************
+     * もだねちゃんの描画処理準備
+    ***************************************/
     // 目と口を出し分けするための乱数用変数を初期化
     int eyeNum       = 1;
     int mouthNum     = 1;
 
 
-    ////////// 描画処理の準備 //////////
+    /***************************************
+     * 描画処理の準備
+    ***************************************/
     time_t prev, now;            // 現在とループ前の時間
     int w, h;                    // 描画する画面の幅・高さ
     int x, y;                    // mvaddstr() などで指定する縦横軸の座標
@@ -405,7 +470,9 @@ int main(void) {
     setlocale(LC_CTYPE, "");     // 2バイト文字を扱えるようにする
 
 
-    ////////// 端末制御の開始 //////////
+    /***************************************
+     * 端末制御の開始
+    ***************************************/
     printf("描画を開始します。\n終了するには Q キーを押してください。\n");
     sleep(3);                // 設定内容表示のため一旦待機
     initscr();               // cursesを初期化。端末制御の開始
@@ -416,7 +483,9 @@ int main(void) {
 
     // 以降はループ処理で毎秒画面を更新して描画を行う
     while(1) {
-        ////////// ループ確認処理 //////////
+        /***************************************
+         * ループ確認処理
+        ***************************************/
         // 前回の時間を確認して1秒経っていたら処理を開始する
         // 同じ場合は指定マイクロ秒スリープして処理をスキップする
         now = time(NULL);
@@ -427,17 +496,24 @@ int main(void) {
         prev = now; // 現在日時を次のループの比較用に格納
 
 
-        ////////// 画面の高さと幅の取得・描画準備 //////////
+        /***************************************
+         * 画面の高さと幅の取得・描画準備
+        ***************************************/
         // 繰り返し毎に高さ幅を取得してレスポンシブにする
         getmaxyx(stdscr, h, w); // 画面の高さと幅の取得。stdscr は端末画面を表す定数。
         erase();                // 画面を消去
 
 
-        ////////// 時計の描画処理 //////////
+        /***************************************
+         * 時計の描画処理
+        ***************************************/
+        // 時間を描画
         printDatetime(1, w - 2, now);
 
 
-        ////////// 天気の描画処理 //////////
+        /***************************************
+         * 天気の描画処理
+        ***************************************/
         // 天気の描画
         printWttr(2, w - 1, wttrLines); // w - 2 でないのは改行文字分を含めるため
 
@@ -448,25 +524,14 @@ int main(void) {
         }
 
 
-        ////////// CO2 グラフの描画処理 //////////
-        // TODO: 10分毎に CO2 濃度の記録と配列の再代入を行う
+        /***************************************
+         * CO2 濃度グラフの描画処理
+        ***************************************/
+        // 10分毎にログへ書き込み & 配列の更新
         if (now % (60 * 10) == 0) {
             doRecordCo2ConceToLogs();
-            getAndAssignCo2Conces(co2Conces);
-
-            // TODO: 作成終わったら消す
-            int i, j, tmp;
-            for (i=0; i<sizeof(co2Conces)/sizeof(co2Conces[0]); i++) {
-                for(j=i+1; j<sizeof(co2Conces)/sizeof(co2Conces[0]); j++) {
-                    if(co2Conces[j] < co2Conces[i]) {
-                        tmp = co2Conces[i];
-                        co2Conces[i] = co2Conces[j];
-                        co2Conces[j] = tmp;
-                    }
-                }
-            }
+            getCo2ConcesFromLogs(co2Conces);
         }
-
 
         // グラフのベースを描画
         printCo2GraphBase(h - 8, w - 35);
@@ -481,7 +546,9 @@ int main(void) {
         doBlinkCo2Graph(h - 4, w - 7, now);
 
 
-        ////////// もだねちゃんの描画処理 //////////
+        /***************************************
+         * もだねちゃんの描画処理
+        ***************************************/
         printModane(h / 2 - 5, 2, eyeNum, mouthNum);
 
         // 4秒毎に次のループ用の乱数を生成
@@ -491,9 +558,14 @@ int main(void) {
         }
 
 
-        ////////// 描画処理 //////////
+        /***************************************
+         * 描画処理後半
+        ***************************************/
         // 画面を再描画
         refresh();
+
+        // TODO: G キーでグラフを拡大/縮小する
+        // TODO: 他のキーだったらヘルプを下に表示する
 
         // Q キーを受け取ったらループを抜ける
         key = getch();
@@ -501,7 +573,7 @@ int main(void) {
             break;
         }
     }
-    endwin(); // 端末制御の終了
 
+    endwin(); // 端末制御の終了
     return 0;
 }
