ebe15e189d96e398decb5e505d095eaa4dea02a5
kenkenpa198 <kenkenpa198@gmail.com>
2023-01-08 10:03:47 +0900

    [update]コメントと空行を調整

--- record_co2_conce.py
+++ record_co2_conce.py
@@ -1,12 +1,12 @@
 import csv
 from datetime import datetime as dt
 import os
+
 import mh_z19
 
 
 def verify_co2_conce(co2_conce: int) -> int:
-    """
-    ■ CO2 濃度を検証する関数
+    """CO2 濃度を検証する関数
     メインプログラム側のメモリサイズ超過などの対策用。
 
     Args:
@@ -15,13 +15,15 @@ def verify_co2_conce(co2_conce: int) -> int:
     Returns:
         int: 正常終了または異常終了を識別する数値。以下いずれかの値が出力される。
             - 0 ... 正常値である
-            - 1 ... 4桁を超える異常値
+            - 1 ... 5桁以上の異常値
             - 2 ... マイナス値である異常値
     """
     if co2_conce > 9999:
         return 1
+
     if co2_conce < 0:
         return 2
+
     return 0
 
 
@@ -37,12 +39,13 @@ co2_conce = mh_z19.read_all()["co2"]
 
 # 取得した値の検証と格納
 if verify_co2_conce(co2_conce) == 1:
-    # 4桁を超える場合は -1 に変更
+    # 5桁以上の場合（異常値）は -1 に変更
     verified_co2_conce = -1
 if verify_co2_conce(co2_conce) == 2:
-    # 負の数値の場合は -2 に変更
+    # 負の数値の場合（異常値）は -2 に変更
     verified_co2_conce = -2
 else:
+    # 正常な値の場合はそのまま代入する
     verified_co2_conce = co2_conce
 
 # logs ディレクトリが無ければ作成
